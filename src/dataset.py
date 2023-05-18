a4bb546ff164215b3dbda6766f1cf3862b26a326
Danil <danil.kononyuk@ya.ru>
2023-05-18 22:27:33 +0800

    initial

---
+++ src/dataset.py
@@ -0,0 +1,59 @@
+import torch
+from datasets import load_dataset
+from torch.utils.data import Dataset
+from transformers import AutoTokenizer
+from torch.utils.data import DataLoader
+
+
+class HuggingDataset(Dataset):
+    def __init__(self, dataset_name, text_field, seq_len, device, separate_token):
+        self.dataset_name = dataset_name
+        self.text_field = text_field
+        self.seq_len = seq_len
+        self.device = device
+        self.separate_token = separate_token
+        self.tokenizer = AutoTokenizer.from_pretrained("ai-forever/rugpt3large_based_on_gpt2")
+        self.data = self._load_data()
+
+    def _load_data(self):
+        dataset = load_dataset(self.dataset_name)["train"]
+        return dataset
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        text = self.data[idx][self.text_field]
+        tokens = self.tokenizer.encode(text, add_special_tokens=False)
+        separate_token_id = self.tokenizer.encode(
+            self.separate_token, add_special_tokens=False
+        )[0]
+
+        while len(tokens) < self.seq_len - 1:
+            tokens.append(separate_token_id)
+            next_text_idx = (idx + 1) % len(self.data)
+            next_text = self.data[next_text_idx][self.text_field]
+            next_tokens = self.tokenizer.encode(next_text, add_special_tokens=False)
+            tokens.extend(next_tokens)
+            idx = next_text_idx
+
+        tokens = tokens[: self.seq_len - 1]
+        tokens.append(separate_token_id)
+
+        input_ids = torch.tensor(tokens, dtype=torch.long).unsqueeze(0).to(self.device)
+
+        return input_ids[0]
+
+
+if __name__ == "__main__":
+    dataset = HuggingDataset(
+        "yelp_review_full",
+        "text",
+        4096,
+        "cuda",
+        "<|endoftext|>",
+    )
+    train_loader = DataLoader(dataset, batch_size=1)
+    for batch in train_loader:
+        print(batch.shape)
+        break
---
+++ src/train.py
@@ -0,0 +1,188 @@
+import torch
+import random
+
+import numpy as np
+from torch.utils.data import DataLoader
+
+from perceiver_ar_pytorch import PerceiverAR
+from perceiver_ar_pytorch.autoregressive_wrapper import AutoregressiveWrapper
+from dataset import HuggingDataset
+
+import argparse
+import wandb
+import os
+
+from rich.table import Table
+from rich.console import Console
+from rich.progress import track
+from rich import box
+
+
+def cycle(loader):
+    while True:
+        for data in loader:
+            yield data
+
+def train(
+    dataset_name,
+    text_field,
+    seq_len,
+    separate_token,
+    batch_size,
+    lr,
+    epochs,
+    device,
+    generate_every,
+    save_every,
+    log_every,
+    output_dir,
+    num_tokens,
+    dim,
+    depth,
+    heads,
+    dim_head,
+    cross_attn_dropout,
+    cross_attn_seq_len,
+    use_wandb,
+    wandb_project,
+):
+    console = Console()
+
+    if use_wandb:
+        wandb.init(project=wandb_project)
+
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    dataset = HuggingDataset(
+        dataset_name,
+        text_field,
+        seq_len,
+        device,
+        separate_token,
+    )
+
+    loader = cycle(DataLoader(dataset, batch_size=batch_size))
+
+    model = PerceiverAR(
+        num_tokens=num_tokens,
+        dim=dim,
+        depth=depth,
+        heads=heads,
+        dim_head=dim_head,
+        cross_attn_dropout=cross_attn_dropout,
+        max_seq_len=seq_len,
+        cross_attn_seq_len=cross_attn_seq_len,
+    )
+
+    model = AutoregressiveWrapper(model)
+    model.to(device)
+
+    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    model_table = Table(
+        title="Model",
+        show_header=True,
+        header_style="bold magenta",
+        box=box.SIMPLE,
+        show_lines=True,
+    )
+    model_table.add_column("Parameter", style="dim", width=12)
+    model_table.add_column("Value", justify="right")
+    model_table.add_row("Num params", f"{n_params:,}")
+    console.print(model_table)
+
+    table = Table(
+        title="Training",
+        show_header=True,
+        header_style="bold magenta",
+        box=box.SIMPLE,
+        show_lines=True,
+    )
+    table.add_column("Epoch")
+    table.add_column("Step")
+    table.add_column("Loss")
+    table.add_column("PPL")
+
+    optim = torch.optim.Adam(model.parameters(), lr=lr)
+    model.train()
+    
+    for epoch in range(epochs):
+        for i in track(range(len(dataset)), description=f"Epoch {epoch+1}/{epochs}"):
+            losses = []
+            for _ in range(4):
+                loss = model(next(loader))
+                loss.backward()
+                losses.append(loss.item())
+            if i % generate_every == 0:
+                model.eval()
+                inp = random.choice(dataset)[:-1]
+
+                sample = model.generate(inp[None, ...], 512)
+                text = dataset.tokenizer.decode(sample[0])
+                if use_wandb:
+                    wandb.log({"text": text, "step": i})
+                console.print(text)
+            if i % save_every == 0:
+                torch.save(model.state_dict(), f"{output_dir}/model_{i}.pt")
+            if i % log_every == 0:
+                table.add_row(str(epoch+1), str(i), f"{sum(losses)/len(losses):.3f}", f"{np.exp(sum(losses)/len(losses)):.3f}")
+                console.print(table)
+                if use_wandb:
+                    wandb.log({"loss": sum(losses)/len(losses), "ppl": np.exp(sum(losses)/len(losses)), "step": i})
+
+            optim.step()
+            optim.zero_grad()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dataset_name", type=str, default="0x7194633/GCRL-dtf")
+    parser.add_argument("--text_field", type=str, default="text")
+    parser.add_argument("--seq_len", type=int, default=4096)
+    parser.add_argument("--separate_token", type=str, default="<|endoftext|>")
+    parser.add_argument("--batch_size", type=int, default=1)
+    parser.add_argument("--lr", type=float, default=1e-4)
+    parser.add_argument("--epochs", type=int, default=10)
+    parser.add_argument("--device", type=str, default="cuda")
+    parser.add_argument("--generate_every", type=int, default=100)
+    parser.add_argument("--log_every", type=int, default=10)
+    parser.add_argument("--save_every", type=int, default=1000)
+    parser.add_argument("--output_dir", type=str, default="output")
+    # Model
+    parser.add_argument("--num_tokens", type=int, default=50400)
+    parser.add_argument("--dim", type=int, default=512)
+    parser.add_argument("--depth", type=int, default=8)
+    parser.add_argument("--heads", type=int, default=8)
+    parser.add_argument("--dim_head", type=int, default=64)
+    parser.add_argument("--cross_attn_dropout", type=int, default=0.5)
+    parser.add_argument("--cross_attn_seq_len", type=int, default=3584)
+    # Logging
+    parser.add_argument("--wandb", action="store_true")
+    parser.add_argument("--wandb_project", type=str, default="text-perceiver")
+
+    args = parser.parse_args()
+
+    train(
+        args.dataset_name,
+        args.text_field,
+        args.seq_len,
+        args.separate_token,
+        args.batch_size,
+        args.lr,
+        args.epochs,
+        args.device,
+        args.generate_every,
+        args.save_every,
+        args.log_every,
+        args.output_dir,
+        args.num_tokens,
+        args.dim,
+        args.depth,
+        args.heads,
+        args.dim_head,
+        args.cross_attn_dropout,
+        args.cross_attn_seq_len,
+        args.wandb,
+        args.wandb_project,
+    )
